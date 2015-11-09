96223a32f932d0ae92d94557c4602245f52d3aa8
sandajith <sandra.thomas@bridge-india.in>
2015-11-09 13:11:26 +0530

    phone number on top link and disabled checkout from top link

---
+++ app/code/local/Mycloset/Membership/Block/Extra/Links.php
@@ -0,0 +1,18 @@
+<?php
+
+class Mycloset_Membership_Block_Extra_Links extends Mage_Core_Block_Template
+{
+      public function addPhoneLink()
+    {       
+        $parentBlock = $this->getParentBlock();
+
+            $text = $this->__('917.684.0609');
+            $parentBlock->addLink(
+                $text, '', $text,
+                true, array('_secure' => true), 40, null,
+                'onclick="javascript:return false;" class="top-link-phone"'
+            );
+
+        return $this;
+    }
+}
