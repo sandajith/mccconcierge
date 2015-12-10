9695d31d5c0f0fe32cd82406b96f6e7f8bebfc43
neenurobin <neenurobin28@gmail.com>
2015-12-10 05:34:23 -0500

    backlog 69 fixed

--- app/code/local/Mycloset/Membership/Block/Membership/Popup.php
+++ app/code/local/Mycloset/Membership/Block/Membership/Popup.php
@@ -11,30 +11,32 @@ class Mycloset_Membership_Block_Membership_Popup extends Mage_Catalog_Block_Prod
 //            Mage::getSingleton('core/session')->setProductOwner($productowner); // set session for product owner
 //       $product['customerid']= Mage::getSingleton('core/session')->getProductOwner();
 //      $customerid = Mage::getSingleton('core/session')->getProductOwner();
-        $model = Mage::getModel('catalog/product'); //getting product model
+        $model = Mage::getModel('catalog/product')->load($product_id); //getting product model
+       
       $product['id'] = $model->load($product_id); //getting product object for particular product id               
-        $product['ShortDescription'] = $model->load($product_id)->getShortDescription(); //product's short description
-        $product['Description'] = $model->load($product_id)->getDescription(); // product's long description
-        $product['Name'] = $model->load($product_id)->getName(); //product name
+        $product['ShortDescription'] = $model->getShortDescription(); //product's short description
+        $product['Description'] = $model->getDescription(); // product's long description
+        $product['Name'] = $model->getName(); //product name
         $product['Price'] = $model->load($product_id)->getPrice(); //product's regular Price
-        $product['SpecialPrice'] = $model->load($product_id)->getSpecialPrice(); //product's special Price
-        $product['ProductUrl'] = $model->load($product_id)->getProductUrl(); //product url
-        $product['ImageUrl'] = $model->load($product_id)->getImageUrl(); //product's image url
-        $product['SmallImageUrl'] = $model->load($product_id)->getSmallImageUrl(); //product's small image url
-        $product['ThumbnailUrl'] = $model->load($product_id)->getThumbnailUrl(); //product's thumbnail image url
-        $product['color'] = $model->load($product_id)->getAttributeText('color'); //product's thumbnail image url
-        $product['sku'] = $model->load($product_id)->getSku(); //product's thumbnail image url      
-        $product['size'] = $model->load($product_id)->getAttributeText('size'); //product's thumbnail image url      
-        $product['designer'] = $model->load($product_id)->getAttributeText('designer'); //product's thumbnail image url
-        $product['product_status'] = $model->load($product_id)->getAttributeText('product_status'); //product's thumbnail image url
-        $product['season'] = $model->load($product_id)->getAttributeText('season'); //product's thumbnail image url
-        $product['shipped_to'] = $model->load($product_id)->getAttributeText('shipped_to'); //product's thumbnail image url      
+        $product['SpecialPrice'] = $model->getSpecialPrice(); //product's special Price
+        $product['ProductUrl'] = $model->getProductUrl(); //product url
+        $product['ImageUrl'] = $model->getImageUrl(); //product's image url
+        $product['SmallImageUrl'] = $model->getSmallImageUrl(); //product's small image url
+        $product['ThumbnailUrl'] = $model->getThumbnailUrl(); //product's thumbnail image url
+        $product['color'] = $model->getAttributeText('color'); //product's thumbnail image url
+        $product['sku'] = $model->getSku(); //product's thumbnail image url      
+        $product['size'] = $model->getAttributeText('size'); //product's thumbnail image url      
+        $product['designer'] = $model->getAttributeText('designer'); //product's thumbnail image url
+        $product['product_status'] = $model->getAttributeText('product_status'); //product's thumbnail image url
+  $product['season'] = $model->getAttributeText('season'); //product's thumbnail image url
+    $product['shipped_to'] = $model['shipped_to']; //product's thumbnail image url      
 //       echo $cats = $model->load($product_id)->getCategoryIds();
 //        foreach ($cats as $categoryId) {
 //            $category = Mage::getModel('catalog/category')->load($categoryId);
 //            $product['category']= $category->getName();
 //        }
 //        print_r($product);
+     
         return $product;
     }
 
--- app/code/local/Mycloset/Membership/controllers/PopupController.php
+++ app/code/local/Mycloset/Membership/controllers/PopupController.php
@@ -68,7 +68,7 @@ class Mycloset_Membership_PopupController extends Mage_Core_Controller_Front_Act
                         jqCustom('.signature').html(data['designer']);
                         jqCustom('.colorBox').html('<a style=" background:' + data['color'] + '"></a>');
                         jqCustom('.description').html(data['Description']);
-                        jqCustom('.status').html('<b>Status</b> : ' + data['product_status']);
+                       jqCustom('.status').html('<b>Status</b> : ' + data['product_status']+' '+data['shipped_to']);
                         jqCustom('.size').html('<i>Size' + data['size'] + '</i>');
                         jqCustom('.season').html('<i>' + data['season'] + '</i>');
                         jqCustom('.tags').html('<div>' + data['tag_details'] + '<span id="remove" class="add add-text"><input type="text" class="inputz" pro="' + data['id'] + '" name="tagss" value=""></span><span id="remove pluse" class="tagpluse"><i class="tagadd">+</i></span></div></div>');
@@ -182,7 +182,7 @@ class Mycloset_Membership_PopupController extends Mage_Core_Controller_Front_Act
                         jqCustom('.signature').html(data['designer']);
                         jqCustom('.colorBox').html('<a style=" background:' + data['color'] + '"></a>');
                         jqCustom('.description').html(data['Description']);
-                        jqCustom('.status').html('<b>Status</b> : ' + data['product_status']);
+                      jqCustom('.status').html('<b>Status</b> : ' + data['product_status']+' '+data['shipped_to']);
                         jqCustom('.size').html('<i>Size' + data['size'] + '</i>');
                         jqCustom('.season').html('<i>' + data['season'] + '</i>');
                          jqCustom('.tags').html('<div>' + data['tag_details'] + '<span id="remove" class="add add-text"><input type="text" class="inputz" pro="' + data['id'] + '" name="tagss" value=""></span><span id="remove pluse" class="tagpluse"><i class="tagadd">+</i></span></div></div>');
@@ -295,7 +295,7 @@ class Mycloset_Membership_PopupController extends Mage_Core_Controller_Front_Act
                         jqCustom('.signature').html(data['designer']);
                         jqCustom('.colorBox').html('<a style=" background:' + data['color'] + '"></a>');
                         jqCustom('.description').html(data['Description']);
-                        jqCustom('.status').html('<b>Status</b> : ' + data['product_status']);
+                       jqCustom('.status').html('<b>Status</b> : ' + data['product_status']+' '+data['shipped_to']);
                         jqCustom('.size').html('<i>Size' + data['size'] + '</i>');
                         jqCustom('.season').html('<i>' + data['season'] + '</i>');
                          jqCustom('.tags').html('<div>' + data['tag_details'] + '<span id="remove" class="add add-text"><input type="text" class="inputz" pro="' + data['id'] + '" name="tagss" value=""></span><span id="remove pluse" class="tagpluse"><i class="tagadd">+</i></span></div></div>');
@@ -408,7 +408,7 @@ class Mycloset_Membership_PopupController extends Mage_Core_Controller_Front_Act
                         jqCustom('.signature').html(data['designer']);
                         jqCustom('.colorBox').html('<a style=" background:' + data['color'] + '"></a>');
                         jqCustom('.description').html(data['Description']);
-                        jqCustom('.status').html('<b>Status</b> : ' + data['product_status']);
+                        jqCustom('.status').html('<b>Status</b> : ' + data['product_status']+' '+data['shipped_to']);
                         jqCustom('.size').html('<i>Size' + data['size'] + '</i>');
                         jqCustom('.season').html('<i>' + data['season'] + '</i>');
                          jqCustom('.tags').html('<div>' + data['tag_details'] + '<span id="remove" class="add add-text"><input type="text" class="inputz" pro="' + data['id'] + '" name="tagss" value=""></span><span id="remove pluse" class="tagpluse"><i class="tagadd">+</i></span></div></div>');
@@ -520,7 +520,7 @@ class Mycloset_Membership_PopupController extends Mage_Core_Controller_Front_Act
                         jqCustom('.signature').html(data['designer']);
                         jqCustom('.colorBox').html('<a style=" background:' + data['color'] + '"></a>');
                         jqCustom('.description').html(data['Description']);
-                        jqCustom('.status').html('<b>Status</b> : ' + data['product_status']);
+                        jqCustom('.status').html('<b>Status</b> : ' + data['product_status']+' '+data['shipped_to']);
                         jqCustom('.size').html('<i>Size' + data['size'] + '</i>');
                         jqCustom('.season').html('<i>' + data['season'] + '</i>');
                         jqCustom('.tags').html('<div>' + data['tag_details'] + '<span id="remove" class="add add-text"><input type="text" class="inputz" pro="' + data['id'] + '" name="tagss" value=""></span><span id="remove pluse" class="tagpluse"><i class="tagadd">+</i></span></div></div>');
@@ -579,22 +579,22 @@ class Mycloset_Membership_PopupController extends Mage_Core_Controller_Front_Act
     public function productAction() {
         $product_id = Mage::app()->getRequest()->getParam('productid');
         $product = array();
-        $model = Mage::getModel('catalog/product'); //getting product model
+        $model = Mage::getModel('catalog/product')->load($product_id); //getting product model
         $product['id'] = $product_id; //getting product object for particular product id               
-        $product['ShortDescription'] = $model->load($product_id)->getShortDescription(); //product's short description
-        $product['Description'] = $model->load($product_id)->getDescription(); // product's long description
-        $product['Name'] = $model->load($product_id)->getName(); //product name
-        $product['Price'] = $model->load($product_id)->getPrice(); //product's regular Price
-        $product['SpecialPrice'] = $model->load($product_id)->getSpecialPrice(); //product's special Price
-        $product['ProductUrl'] = $model->load($product_id)->getProductUrl(); //product url
-        $product['ImageUrl'] = $model->load($product_id)->getImageUrl(); //product's image url
-        $product['color'] = $model->load($product_id)->getAttributeText('color'); //product's thumbnail image url
-        $product['sku'] = $model->load($product_id)->getSku(); //product's thumbnail image url      
-        $product['size'] = $model->load($product_id)->getAttributeText('size'); //product's thumbnail image url      
-        $product['designer'] = $model->load($product_id)->getAttributeText('designer'); //product's thumbnail image url
-        $product['product_status'] = $model->load($product_id)->getAttributeText('product_status'); //product's thumbnail image url
-        $product['season'] = $model->load($product_id)->getAttributeText('season'); //product's thumbnail image url
-        $product['shipped_to'] = $model->load($product_id)->getAttributeText('shipped_to'); //product's thumbnail image url  
+        $product['ShortDescription'] = $model->getShortDescription(); //product's short description
+        $product['Description'] = $model->getDescription(); // product's long description
+        $product['Name'] = $model->getName(); //product name
+        $product['Price'] = $model->getPrice(); //product's regular Price
+        $product['SpecialPrice'] = $model->getSpecialPrice(); //product's special Price
+        $product['ProductUrl'] = $model->getProductUrl(); //product url
+        $product['ImageUrl'] = $model->getImageUrl(); //product's image url
+        $product['color'] = $model->getAttributeText('color'); //product's thumbnail image url
+        $product['sku'] = $model->getSku(); //product's thumbnail image url      
+        $product['size'] = $model->getAttributeText('size'); //product's thumbnail image url      
+        $product['designer'] = $model->getAttributeText('designer'); //product's thumbnail image url
+        $product['product_status'] = $model->getAttributeText('product_status'); //product's thumbnail image url
+        $product['season'] = $model->getAttributeText('season'); //product's thumbnail image url
+        $product['shipped_to'] = $model['shipped_to']; //product's thumbnail image url  
         $prefix = Mage::getConfig()->getTablePrefix();
         $resource = Mage::getSingleton('core/resource');
         $readConnection = $resource->getConnection('core_read');
