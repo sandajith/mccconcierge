3161dfc55880253085b93c7648839581e88f647e
neenurobin <neenurobin28@gmail.com>
2015-12-11 04:59:49 -0500

    backlog 149, 150

--- app/code/local/Mycloset/Membership/Block/Extra/Links.php
+++ app/code/local/Mycloset/Membership/Block/Extra/Links.php
@@ -9,9 +9,9 @@ class Mycloset_Membership_Block_Extra_Links extends Mage_Core_Block_Template
             $text = $this->__('917.684.0609');
 //            $url=$this->__('tel:917.684.0609');
             $parentBlock->addLink(
-                $text,'', $text,
-                true, array('callto:917.684.0609'), 40, null,
-                'onclick="javascript:return false;" class="top-link-phone" '
+                $text,'tel:917.684.0609', $text,
+                false, null, 40, null,
+                'class="top-link-phone" '
             );
 
         return $this;
